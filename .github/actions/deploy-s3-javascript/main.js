0c44dd5d988df9aec5bb885d40a3132c1f762ada
dpastormorato <dpastormorato@yahoo.ie>
2025-05-18 13:55:39 +1000

    Added new custom actions v10

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-  core.notice('Hellow from my custom JS Action!');
+  // Get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // Upload some files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  core.notice('Hello from my custom JS Action!');
 }
 
 run();
